6ea55e7ac1bcaaada9df385f990a143813866376
Mohammed Al-Taie <altaiem888@gmail.com>
2021-10-19 10:18:00 -0400

    Initial commit

---
+++ app/src/main/java/com/elecCoen_390_projectgroup_3/g_track/MainActivity.java
@@ -0,0 +1,14 @@
+package com.elecCoen_390_projectgroup_3.g_track;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
