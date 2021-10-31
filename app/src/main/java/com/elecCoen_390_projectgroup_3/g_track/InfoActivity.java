29a083036d46bf708f175ff5115b67b554f98c43
paulwestenberg <67531234+paulwestenberg@users.noreply.github.com>
2021-10-30 23:37:09 -0400

    Created sensor reading mechanism for list in database entitles 'sensor'

--- app/src/main/java/com/elecCoen_390_projectgroup_3/g_track/InfoActivity.java
+++ app/src/main/java/com/elecCoen_390_projectgroup_3/g_track/InfoActivity.java
@@ -1,5 +1,6 @@
 package com.elecCoen_390_projectgroup_3.g_track;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +9,23 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
     protected FloatingActionButton addBinFloatingButton;
+    private ListView listview;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,6 +49,8 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         addBinFloatingButton=findViewById(R.id.floatingActionButtonAddBin);
+        listview = findViewById(R.id.BinListViewid);
+
         addBinFloatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +59,29 @@ public class InfoActivity extends AppCompatActivity {
                 dialog.show(getSupportFragmentManager(),"InsertBinDialogFragment");
             }
         });
+
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this , R.layout.list_item , list);
+
+        listview.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("sensor");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
 
